8341ddd387f6f9aacb563b5e6c8fb2e8e84acdbb
brut4luk3 <lucasreinert96@gmail.com>
2022-06-01 11:37:11 -0300

    Página de login completa (layout)

--- key_lizard/urls.py
+++ key_lizard/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registration/', include('django.contrib.auth.urls'))
+    path('registration/', include('django.contrib.auth.urls')),
+    path('usuarios/', include('usuarios.urls'))
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 
+from . import views
+
+app_name = 'usuarios'
+
+urlpatterns =[
+    path('', views.index, name='index')
+]
+
