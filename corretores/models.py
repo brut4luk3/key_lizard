96318bd9e1bc8de1f7b314163edcd68e82d21f33
brut4luk3 <lucasreinert96@gmail.com>
2022-06-08 20:02:03 -0300

    Mock da página inicial de Equipe 100%

--- corretores/models.py
+++ corretores/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Corretor(models.Model):
-    nome = models.CharField(max_length=200)
-    sobrenome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=30)
+    sobrenome = models.CharField(max_length=50)
 
     def __str__(self):
         return self.nome
