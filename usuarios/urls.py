ca2635f2720ec3bddb88d97e929589d1d1ffdb56
brut4luk3 <lucasreinert96@gmail.com>
2022-07-04 18:38:38 -0300

    Devolução de chaves 100%

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,6 +7,8 @@ app_name = 'usuarios'
 urlpatterns =[
     path('', views.index, name='index'),
     path('retirada/', views.selecionar_chave, name='selecionar_chave'),
-    path('retirada/<int:chave_id>/', views.registrar_retirada, name='registrar_retirada')
+    path('retirada/<int:chave_id>/', views.registrar_retirada, name='registrar_retirada'),
+    path('devolucao/', views.selecionar_chave_devolucao, name='selecionar_chave_devolucao'),
+    path('devolucao/<int:chave_id>/', views.registrar_devolucao, name='registrar_devolucao')
 ]
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from chaves.models import Chave
 from setores.models import Setor
+from corretores.models import Corretor
+from chaves.models import Chave
 import datetime
 
 
@@ -61,3 +62,49 @@ def registrar_retirada(request, chave_id):
 
         return render(request, 'retirada/sucesso_retirada.html')
 # FUNC DE RETIRADA - FIM #
+
+# FUNC DE SELEÇÃO - DEVOLUÇÃO #
+def selecionar_chave_devolucao(request):
+
+    id_vendas = Setor.objects.get(nome_setor='Vendas')
+    id_locacao = Setor.objects.get(nome_setor='Locação')
+
+    chaves_cadastradas = request.user.chave_set.all()
+    chaves_vendas = Chave.objects.filter(setor_atribuido=id_vendas).all()
+    chaves_locacao = Chave.objects.filter(setor_atribuido=id_locacao).all()
+
+    context = {
+        'chaves_cadastradas': chaves_cadastradas,
+        'chaves_vendas': chaves_vendas,
+        'chaves_locacao': chaves_locacao
+    }
+
+    return render(request, 'devolucao/selecionar_chave_devolucao.html', context=context)
+# FUNC DE SELEÇÃO - DEVOLUÇÃO - FIM #
+
+# FUNC DE DEVOLUÇÃO #
+def registrar_devolucao(request, chave_id):
+
+    if request.method == 'GET':
+
+        chave = get_object_or_404(Chave, pk=chave_id)
+        setores_cadastrados = request.user.setor_set.all()
+        corretores_cadastrados = request.user.corretor_set.all()
+
+        context = {
+            'chave': chave,
+            'setores_cadastrados': setores_cadastrados,
+            'corretores_cadastrados': corretores_cadastrados
+        }
+
+        return render(request, 'devolucao/registrar_devolucao.html', context=context)
+
+    elif request.method == 'POST':
+
+        horario_atualizado = datetime.datetime.now()
+        id_imobiliaria = Corretor.objects.get(nome='Imobiliária')
+
+        chaves_devolvidas = Chave.objects.filter(pk=chave_id).update(corretor=id_imobiliaria, horario=horario_atualizado)
+
+        return render(request, 'devolucao/sucesso_devolucao.html')
+# FUNC DE DEVOLUÇÃO - FIM #
